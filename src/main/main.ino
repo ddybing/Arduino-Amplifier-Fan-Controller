4e02ff0e8f54870c6ba8c1111f132f3344b1e306
Daniel Dybing <daniel.dybing@gmail.com>
2020-02-17 15:53:02 +0100

    Added automatic fan control.
    Fans now adjust according to temperature, and turn on/off when above/under the minimum temperature specified. Speed compensation for individual fans can now also be set

--- src/main/main.ino
+++ src/main/main.ino
@@ -5,6 +5,7 @@
  * PIN 2  = Switch
  * PIN 3  = Fan 1 PWM
  * PIN 5  = Fan 2 PWM
+ * PIN 6  = LED PWM
  * PIN 7  = LED relay
  * PIN 8  = Fan 1 relay
  * PIN 12 = Signal from temp sensor (DS18B20)
@@ -17,6 +18,7 @@
 // 0.625 * pow (temp, 2) + 0 + 30 = duty_cycle
 // E.g. temp = 30, duty_cycle = 86.25
 
+
 #include <OneWire.h>
 #include <math.h>
 #include <FastLED.h>
@@ -28,17 +30,30 @@
 #define SWITCH_PIN 2
 #define FAN1_CONTROL_PIN 3
 #define FAN2_CONTROL_PIN 5
+#define LED_CONTROL_PIN 6
 #define LED_SWITCH_PIN 7
 #define FAN1_SWITCH_PIN 8
 #define TEMPSENSOR_PIN 12
 #define FAN2_SWITCH_PIN 13
 
+#define ONE_WIRE_BUS 12
 
-// Temp sensor addresses
-DeviceAddress tempSensor1 = {0x28, 0xAA, 0xB3, 0xB0, 0x16, 0x13, 0x02, 0x83}; // For vifte 1
-DeviceAddress tempSensor2 = {0x28, 0xAA, 0xA5, 0x84, 0x13, 0x13, 0x02, 0x10}; // For vifte 2
+//DeviceAddress tempSensor1[8] = {0x28, 0xAA, 0xB3, 0xB0, 0x16, 0x13, 0x02, 0x83};
+//DeviceAddress tempSensor2[8] = {0x28, 0xAA, 0xA5, 0x84, 0x13, 0x13, 0x02, 0x10};
 
 
+
+// Settings ||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+#define MIN_TEMP 28
+#define MAX_TEMP 60
+
+#define FAN1_SPEED_COMP 1 // Speed compensation for fan 1. 1 = no compensation (duty_cycle x 1)
+#define FAN2_SPEED_COMP 1 // Speed compensation for fan 2. 1 = no compensation (duty_cycle x 1)
+
+
+// End of Settings |||||||||||||||||||||||||||||||||||||||||||||
 
 // Classes ||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
@@ -59,8 +74,9 @@ void Fan::turnOff()
 {
   // Set fan speed to 10% duty cycle. 
   analogWrite (signal_pin, 25.5);
-  delay (100); // Wait for 100 milliseconds
+  delay (100); // Wait for 100 milliseconds.
   digitalWrite (switch_pin, HIGH);
+  delay(100); // Wait for relay to turn off.
 }
 
 
@@ -68,8 +84,52 @@ void Fan::turnOn()
 {
   // Set fan speed to 10% duty cycle.
   analogWrite (signal_pin, 25.5);
-  delay (100); // Wait for 100 milliseconds
+  delay (100); // Wait for 100 milliseconds.
   digitalWrite (switch_pin, LOW);
+  delay(100); // Wait for relay to turn on.
+}
+
+
+
+class LED
+{
+  public:
+
+        int switch_pin;
+        int signal_pin;
+        int state;
+
+        void turnOff();
+
+        void turnOn();
+
+        void changeColour();
+        
+};
+
+
+void LED::turnOff()
+{
+  // Set color to black
+  // ....
+  delay(100); // Wait 100 milliseconds
+  digitalWrite(switch_pin, HIGH); // Turn off relay.
+  delay(100); // Wait for relay to turn off.
+}
+
+void LED::turnOn()
+{
+  // Set color to black
+  // ....
+  delay(100); // Wait 100 milliseconds
+  digitalWrite(switch_pin, HIGH);
+  delay(100); // Wait for the relay to turn on. 
+  
+}
+
+void LED::changeColour()
+{
+  
 }
 // End of Classes ||||||||||||||||||||||||||||||||||||||
 
@@ -91,8 +151,7 @@ void setup(void)
  // Open serial port at 9600 baud. 
  Serial.begin(9600); 
 
- //DeviceAddress tempSensor1 = {0x28, 0xAA, 0xB3, 0xB0, 0x16, 0x13, 0x02, 0x83};
- //DeviceAddress tempSensor2 = {0x28, 0xAA, 0xA5, 0x84, 0x13, 0x13, 0x02, 0x10};
+
  // -- Define and initialize I/O pins -- 
 
  // Fan 1 relay state
@@ -120,6 +179,8 @@ void setup(void)
  Serial.print("### Arduino Receiver Cooling and Visualization System (A.R.C.V.S) ###");
  Serial.print("\n");
 
+ digitalWrite(FAN1_SWITCH_PIN, LOW);
+ digitalWrite(FAN2_SWITCH_PIN, LOW);
 
 
 } 
@@ -133,14 +194,18 @@ Fan fan1;
 
 void setFanSpeed(float fanSpeed)
 {
-      analogWrite(FAN1_CONTROL_PIN, fanSpeed);
+      analogWrite(FAN1_CONTROL_PIN, (fanSpeed * FAN1_SPEED_COMP));
+      analogWrite(FAN2_CONTROL_PIN, (fanSpeed * FAN2_SPEED_COMP));
 }
 
 
 float getTemperature()
 {
   sensors.requestTemperatures();
+  delay(100);
   float temp = sensors.getTempCByIndex(0);
+  Serial.print("Temp: ");
+  Serial.println(temp);
   return temp;
 }
 
@@ -148,17 +213,40 @@ float getTemperature()
 float calculateDutyCycle(float temp)
 {
   // Return duty cycle for fan PWM (0-255).
-  return 0.625 * pow (temp, 2) + 0 + 30;
+  if (temp >MAX_TEMP){
+    return 255;
+  }
+  else {
+  return 0.0625 * (temp * temp) + 0 + 30;
+  }
 }
 
 
-void loop(void) 
+void loop(void)
 { 
+  
   float temp = getTemperature();
   float duty_cycle = calculateDutyCycle(temp);
-  setFanSpeed(duty_cycle);
-  
-  
+  if (temp > MAX_TEMP) 
+  {
+    setFanSpeed(255);
+  }
 
+  else if (temp < MIN_TEMP)
+  {
+    digitalWrite(FAN1_SWITCH_PIN, HIGH); //fan1.turnOff();
+    digitalWrite(FAN2_SWITCH_PIN, HIGH); //fan2.turnOff();
     
+  }
+  else 
+  {
+    digitalWrite(FAN1_SWITCH_PIN, LOW); // fan1.turnOn();
+    digitalWrite(FAN2_SWITCH_PIN, LOW); // fan2.turnOn();
+    setFanSpeed(duty_cycle);
+  }
+
+  Serial.print("Duty cycle: ");
+  Serial.println(duty_cycle);
+  delay(1000);
+      
 }
